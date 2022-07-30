411c90a1fefc4e6ef859ddd6b08f25b983cc83c2
Sirasit Thitirattanakorn <sirasit@siruxsolutions.com>
2022-07-30 13:59:52 -0700

    init: create project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ParkRoundedIcon from '@mui/icons-material/ParkRounded';
+function Header() {
+    return (
+        <header>
+            <h1>
+                <ParkRoundedIcon />
+                Nurture List
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
